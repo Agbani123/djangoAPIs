91b1571c722772ed3420bf43cac39d5842583a51
Agbani123 <okaforjulia2016@gmail.com>
2022-07-09 10:38:57 +0100

    Changes to model, urls,serializers

--- ChidinmaOkafor/links/views.py
+++ ChidinmaOkafor/links/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from .serializers import LinkSerializer
+from .models import Links
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+
 
 # Create your views here.
+class PostListApi(generics.ListAPIView):
+    queryset=Links.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+ 
+class PostCreateApi(generics.CreateAPIView):
+    queryset = Links.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+ 
+class PostDetailApi(generics.RetrieveAPIView):
+    queryset = Links.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateApi(generics.UpdateAPIView): 
+    queryset = Links.objects.filter(active=True)
+    serializer_class = LinkSerializer
+ 
+
+class PostDeleteApi(generics.DestroyAPIView):
+    queryset= Links.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
